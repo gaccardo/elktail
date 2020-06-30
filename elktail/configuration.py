d86d41eb0c99d7cc7e89d61fb003eb172acecfd5
Guido Accardo <gaccardo@gmail.com>
2020-06-30 17:54:29 -0300

    config generator

--- elktail/configuration.py
+++ elktail/configuration.py
@@ -10,7 +10,12 @@ def get_config():
     )
 
     if not os.path.exists(config_path):
-        print("must create a config file first")
+        print("your elktail is not configured")
+        opt = str(input("would you like to configure it now? <Y/n>: "))
+
+        if opt in ['Y', 'y', 'yes', 'YES']:
+            config_creator(config_path)
+
         sys.exit(-1)
 
     config = configparser.ConfigParser()
@@ -22,3 +27,25 @@ def get_config():
         'password': config['default']['password'],
         'port': int(config['default']['port'])
     }
+
+
+def config_creator(config_path):
+    print(f"creating configuration file: {config_path}")
+    host = input("elasticsearch host: ")
+    username = input("username: ")
+    password = input("password: ")
+    scheme = input("scheme (HIGLY recommended https): ")
+    port = input("port: ")
+
+    config = configparser.RawConfigParser()
+    config.add_section("default")
+    config.set("default", "host", host)
+    config.set("default", "username", username)
+    config.set("default", "password", password)
+    config.set("default", "scheme", scheme)
+    config.set("default", "port", port)
+
+    with open(config_path, 'w') as configfile:
+        config.write(configfile)
+
+    print("elktail configured")
